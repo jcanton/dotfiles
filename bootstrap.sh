93806c6f8c0ab47e16c6c19899668124bc95258c
Jacopo <jacopo.canton@gmail.com>
2025-03-12 18:02:00 +0100

    wip8

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,24 @@
 function link_item() {
 	item=$1
 	target=$2
+	today=$(date +%Y%m%d)
+	originals_dir="$(pwd)/originals_${today}"
+	mkdir -p "${originals_dir}"
 	if [ -e "${target}" ]; then
-		command="mv ${target} ${target}.orig_dot"
-		echo "$command"
+		if [ ! -L "${target}" ] || [ "$(readlink "${target}")" != "${item}" ]; then
+			command="mv ${target} ${originals_dir}/"
+			echo "$command"
+		else
+			echo "$target is already linked to $item"
+			echo ""
+			return
+		fi
+	else
+		echo "$target does not exist"
 	fi
 	command="ln -s ${item} ${target}"
-	echo "$command"
+	echo "    $command"
+	echo ""
 }
 
 function deploy_dotfiles() {
@@ -20,17 +32,20 @@ function deploy_dotfiles() {
 		link_item "$(pwd)/${item}" "${HOME}/${item}"
 	done
 
-	for item in conditionals/.* conditionals/*; do
-		read -r -p "Enter the name to link ${item} in the home directory (leave empty to skip): " link_name
-		if [ -z "${link_name}" ]; then
-			read -r -p "No name provided. Skip linking ${item}? (y/n) " -n 1
-			echo ""
-			if [[ $REPLY =~ ^[Yy]$ ]]; then
-				continue
+	for item in conditionals/* conditionals/.*; do
+		[[ -e "$item" ]] || continue  # Skip the directory itself
+		while true; do
+			read -r -p "Enter the name to link ${item} in the home directory (leave empty to skip): " link_name
+			if [ -z "${link_name}" ]; then
+				read -r -p "No name provided. Skip linking ${item}? (y/n) " -n 1
+				if [[ $REPLY =~ ^[Yy]$ ]]; then
+					break
+				fi
+			else
+				link_item "$(pwd)/${item}" "${HOME}/${link_name}"
+				break
 			fi
-		else
-			link_item "$(pwd)/${item}" "${HOME}/${link_name}"
-		fi
+		done
 	done
 
 	source "${HOME}/.bash_profile";
@@ -40,6 +55,7 @@ function deploy_dotfiles() {
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit;
 git pull origin main;
 
+echo ""
 deploy_dotfiles;
 
 unset deploy_dotfiles;
