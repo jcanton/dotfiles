41621af3eb62e95af532b2f6be818532bc97db6b
Jacopo <jacopo.canton@gmail.com>
2025-03-14 18:26:40 +0100

    change term to tokyonight

--- conditionals/.bashrc.local.pi.sh
+++ conditionals/.bashrc.local.pi.sh
@@ -2,7 +2,8 @@
 
 # Base16 Shell
 export CLICOLOR=1
-source $HOME/projects/dotfiles/base16/base16-solarized-light.sh
+#source $HOME/projects/dotfiles/base16/base16-solarized-dark.sh
+source $HOME/projects/tinted-shell/scripts/base16-tokyo-night-terminal-storm.sh
 
 # gt4py
 export GT4PY_BUILD_CACHE_LIFETIME=PERSISTENT
@@ -10,8 +11,8 @@ export GT4PY_BUILD_CACHE_DIR=/home/jcanton/projects/
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 set_brightness() {
     brightness_value=$1
