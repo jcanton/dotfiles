c2d93dfe805d2dacfb1acf9d3d7ab3ccc8fede66
Jacopo <jacopo.canton@gmail.com>
2025-03-15 22:28:51 +0100

    small edits

--- dots/.config/nvim/lua/config/keymaps.lua
+++ dots/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--------------------------------------------------------------------------------
+--- The Primeagen --------------------------------------------------------------
+--- https://www.youtube.com/watch?v=w7i4amO_zaE --------------------------------
+--------------------------------------------------------------------------------
+
+-- J joins lines but keeps the cursor where it is
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Use the void register for pasting with <leader>p
+vim.keymap.set("x", "<leader>p", '"_dP')
