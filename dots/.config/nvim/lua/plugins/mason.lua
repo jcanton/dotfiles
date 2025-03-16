abfa0ea23f55384fdf1c65c560a65c942dca2277
Jacopo <jacopo.canton@gmail.com>
2025-03-16 11:21:11 +0100

    these get loaded and installed with lazyvimExtras

--- dots/.config/nvim/lua/plugins/mason.lua
+++ dots/.config/nvim/lua/plugins/mason.lua
@@ -2,11 +2,6 @@ return {
     "williamboman/mason.nvim",
     opts = {
         ensure_installed = {
-            "pyright",
-            "ruff",
-            "mypy",
-            "bash-language-server",
-            "shfmt",
             "fortls",
         },
     },
