601df24d533787629827457b4e8d0c87c2dabf43
Jacopo <jacopo.canton@gmail.com>
2025-03-24 16:38:13 +0100

    Don't autostart anything

--- conditionals/i3/startup_programs.sh
+++ conditionals/i3/startup_programs.sh
@@ -3,9 +3,10 @@
 # 1
 # i3-msg 'workspace 1:loc; exec chromium'
 # sleep 5
-i3-msg 'workspace 1:loc; exec /home/jcanton/.local/bin/obsidian'
-sleep 5
+# i3-msg 'workspace 1:loc; exec /home/jcanton/.local/bin/obsidian' sleep 5
+# sleep 5
 #i3-msg 'workspace 1:loc; exec chromium --profile-directory=Default --app-id=pjibgclleladliembfgfagdaldikeohf'
+# sleep 5
 i3-msg 'workspace 1:loc; exec i3-sensible-terminal'
 #sleep 5
 
