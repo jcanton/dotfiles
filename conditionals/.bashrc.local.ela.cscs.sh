71594e4bed8ad043b43c360ac767b5ae9fb8bd3f
Jacopo <jacopo.canton@gmail.com>
2025-04-04 15:37:45 +0200

    update balfrin bashrc

--- conditionals/.bashrc.local.ela.cscs.sh
+++ conditionals/.bashrc.local.ela.cscs.sh
@@ -13,14 +13,14 @@ if [ -n "${VSCODE_INVOKING}" ]; then
     # Base16 Shell
     source $HOME/projects/tinted-shell/scripts/base16-solarized-light.sh
 fi
-export GT4PY_BUILD_CACHE_LIFETIME=PERSISTENT
 
-export OMP_NUM_THREADS=12
 alias sq='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
 alias sql='squeue -u jcanton -o "%.8i %.8u %.7a %.9P %.50j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E"'
 function wsq {
     watch -n 1 "squeue -u jcanton -o '%.8i %.8u %.7a %.9P %.30j %.8T %.13S %.10M %.10L %.6D %.5C %.11r %E'"
 }
+
+export GT4PY_BUILD_CACHE_LIFETIME=PERSISTENT
 
 case $HOSTNAME in
     balfrin*)
@@ -50,20 +50,3 @@ loadUenv() {
     #uenv start /scratch/mch/leclairm/uenvs/images/icon.v1.rc4.sqfs
     #uenv view icon-wcp:icon
 }
-
-# # PALM
-# export LD_LIBRARY_PATH="/store/g142/jcanton/repos/palm_build/rrtmg/lib:${LD_LIBRARY_PATH}"
-# export PATH="/store/g142/jcanton/repos/palm_build/bin:${PATH}"
-
-# # MCH stuff
-# # source /users/osm/.opr_setup_dir
-# # export PATH=$OPR_SETUP_DIR/bin:$PATH
-# # export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
-# ulimit -s unlimited # for not getting segmentation faults
-# export OMP_STACKSIZE=500M
-# export OMP_PLACES=sockets
-# export OMP_PROC_BIND="spread,close,close"
-# export PATH="/project/s83c/fieldextra/daint/bin:$PATH"
-# export PATH="/project/s83c/fieldextra/daint/tools:$PATH"
-# alias fieldextra='/project/s83c/fieldextra/daint/bin/fieldextra_gnu_opt_omp'
-# alias grins='/project/s83c/fieldextra/daint/tools/grins'
