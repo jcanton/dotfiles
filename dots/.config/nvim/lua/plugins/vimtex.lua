5e4e4822579254ec3bf46e9cc572675f6db2028a
Jacopo <jacopo.canton@gmail.com>
2025-06-25 11:34:31 +0200

    skim viewer

--- dots/.config/nvim/lua/plugins/vimtex.lua
+++ dots/.config/nvim/lua/plugins/vimtex.lua
@@ -4,6 +4,7 @@ return {
     config = function()
         vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
         vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
+        vim.g.vimtex_view_method = "skim"
     end,
     init = function()
         vim.g.vimtex_compiler_latexmk = {
