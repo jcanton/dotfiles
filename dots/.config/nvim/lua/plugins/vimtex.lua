e758cf964aed86b14adc33fff30b12ed54839612
Jacopo <jacopo.canton@gmail.com>
2025-06-25 11:06:12 +0200

    update vimtex

--- dots/.config/nvim/lua/plugins/vimtex.lua
+++ dots/.config/nvim/lua/plugins/vimtex.lua
@@ -5,6 +5,17 @@ return {
         vim.g.vimtex_mappings_disable = { ["n"] = { "K" } } -- disable `K` as it conflicts with LSP hover
         vim.g.vimtex_quickfix_method = vim.fn.executable("pplatex") == 1 and "pplatex" or "latexlog"
     end,
+    init = function()
+        vim.g.vimtex_compiler_latexmk = {
+            options = {
+                "-verbose",
+                "-file-line-error",
+                "-synctex=1",
+                "-interaction=nonstopmode",
+                "-shell-escape",
+            },
+        }
+    end,
     keys = {
         { "<localLeader>l", "", desc = "+vimtex", ft = "tex" },
         { "<localLeader>ll", "<ESC>:w<CR><cmd>VimtexCompileSS<cr>", desc = "vimtex-compileSS", ft = "tex" },
