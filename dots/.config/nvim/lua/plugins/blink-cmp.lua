8a63a2c41cf2bad65c26ed00ac71d2a61d854763
Jacopo <jacopo.canton@gmail.com>
2025-07-09 10:34:21 +0200

    make autocomplete toggleable

--- dots/.config/nvim/lua/plugins/blink-cmp.lua
+++ dots/.config/nvim/lua/plugins/blink-cmp.lua
@@ -37,4 +37,23 @@ return {
         },
     },
     opts_extend = { "sources.default" },
+    -- Make blink.cmp toogleable
+    opts = function(_, opts)
+        vim.b.completion = true
+
+        Snacks.toggle({
+            name = "Completion",
+            get = function()
+                return vim.b.completion
+            end,
+            set = function(state)
+                vim.b.completion = state
+            end,
+        }):map("<leader>uk")
+
+        opts.enabled = function()
+            return vim.b.completion ~= false
+        end
+        return opts
+    end,
 }
