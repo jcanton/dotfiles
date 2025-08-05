19056a988ea3e4b75774558d93cfc2f01f624301
Jacopo <jacopo.canton@gmail.com>
2025-08-05 14:25:24 +0200

    telescope seems to be slow after all

--- dots/.config/nvim/lua/plugins/snacks.lua
+++ dots/.config/nvim/lua/plugins/snacks.lua
@@ -2,53 +2,52 @@ return {
     "folke/snacks.nvim",
     ---@type snacks.Config
     opts = {
-    dashboard = {
+        dashboard = {
             sections = {
                 { section = "header" },
                 { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
                 { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
                 { section = "startup" },
             },
-    },
-        -- try neo-tree and telescope
-        -- picker = {
-        --     sources = {
-        --         -- explorer picker configuration
-        --         explorer = {
-        --             win = {
-        --                 list = {
-        --                     keys = {
-        --                         ["o"] = "confirm", -- remap
-        --                     },
-        --                 },
-        --             },
-        --         },
-        --         -- file picker configuration
-        --         files = {
-        --             -- search hidden files too
-        --             hidden = true,
-        --             win = {
-        --                 input = {
-        --                     keys = {
-        --                         ["H"] = "toggle_hidden",
-        --                     },
-        --                 },
-        --             },
-        --         },
-        --         -- grep picker configuration
-        --         grep = {
-        --             -- search hidden files too
-        --             hidden = true,
-        --             win = {
-        --                 input = {
-        --                     keys = {
-        --                         ["H"] = "toggle_hidden",
-        --                     },
-        --                 },
-        --             },
-        --         },
-        --     },
-        -- },
+        },
+        picker = {
+            sources = {
+                -- explorer picker configuration
+                explorer = {
+                    win = {
+                        list = {
+                            keys = {
+                                ["o"] = "confirm", -- remap
+                            },
+                        },
+                    },
+                },
+                -- file picker configuration
+                files = {
+                    -- search hidden files too
+                    hidden = true,
+                    win = {
+                        input = {
+                            keys = {
+                                ["H"] = "toggle_hidden",
+                            },
+                        },
+                    },
+                },
+                -- grep picker configuration
+                grep = {
+                    -- search hidden files too
+                    hidden = true,
+                    win = {
+                        input = {
+                            keys = {
+                                ["H"] = "toggle_hidden",
+                            },
+                        },
+                    },
+                },
+            },
+        },
     },
     -- keys = {
     --     -- stylua: ignore
