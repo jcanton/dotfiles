b6cf3d8d552ade216eeb45bb1a0af5918c724713
Jacopo <jacopo.canton@gmail.com>
2025-10-03 16:03:17 +0200

    updates

--- dots/.config/nvim/lua/plugins/mason-workaround.lua
+++
@@ -1,6 +0,0 @@
-return {
-    -- pin older package versions which otherwise crash everything
-    -- https://github.com/LazyVim/LazyVim/issues/6039
-    { "mason-org/mason.nvim", version = "^1.0.0" },
-    { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
-}
