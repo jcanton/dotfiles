be43b79d216c9f0960c4a99608da33e3b94c73d3
Jacopo <jacopo.canton@gmail.com>
2026-05-12 09:28:20 +0200

    bun

--- conditionals/.bashrc.local.mac.sh
+++ conditionals/.bashrc.local.mac.sh
@@ -21,6 +21,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
 # Skim
 alias skim="/Applications/Skim.app/Contents/MacOS/Skim"
 
